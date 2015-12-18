8fee0d35d48e21abcda8ce8ac0fc5f31635171af
Matthew Lawton <mlawton@udel.edu>
2015-12-18 16:19:45 -0500

    initial commit

---
+++ FinalExamQuestions/src/QuestionC/IllegalRectangle.java
@@ -0,0 +1,20 @@
+package QuestionC;
+
+public class IllegalRectangle extends Exception {
+	private int x;
+	private int y;
+	
+	public IllegalRectangle(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+
+}
